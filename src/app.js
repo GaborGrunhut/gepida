8a55490cc6a827454265b6afb19060eadbd6c96b
Grünhut Gábor <grunhutg@yahoo.com>
2023-03-14 18:20:01 +0100

    Kezdés

--- src/app.js
+++ src/app.js
@@ -3,8 +3,8 @@
 * Author: Grünhut Gábor
 * Copyright: 2023, Grünhut Gábor
 * Group: Szoft I-1/E
-* Date: 2023-02-07
-* Github: https://github.com/GaborGrunhut/
+* Date: 2023-03-14
+* Github: https://github.com/GaborGrunhut/gepida.git
 * Licenc: GNU GPL
 */
 
