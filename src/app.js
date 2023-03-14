cb086c8b2feb35e95fc9e00a7d6144d860b7f797
Grünhut Gábor <grunhutg@yahoo.com>
2023-03-14 19:59:16 +0100

    style.css fájl tartalma frissítve.

--- src/app.js
+++ src/app.js
@@ -8,3 +8,32 @@
 * Licenc: GNU GPL
 */
 
+const bicycles = document.querySelector('#bicycles');
+const bicyclesArray = [
+    {bicName: "cassis", bicWheel: 28, bicUsage: "offroad", bicPrice: 557900},
+    {bicName: "Alboin 900", bicWheel: 28, bicUsage: "trekking", bicPrice: 519900},
+    {bicName: "Asgard", bicWheel: 29, bicUsage: "technikás utak", bicPrice: 519900},
+    {bicName: "Ruga", bicWheel: 29, bicUsage: "hegyi", bicPrice: 372900},
+    {bicName: "Reptila", bicWheel: 28, bicUsage: "városi", bicPrice: 308900},
+    {bicName: "Sirmium", bicWheel: 29, bicUsage: "hegyi", bicPrice: 264900}
+]
+
+bicyclesArray.forEach((bicycle) => {
+    
+    let tr = document.createElement('tr');
+    let tdBicName = document.createElement('td');
+    let tdBicWheel = document.createElement('td');
+    let tdBicUsage = document.createElement('td');
+    let tdBicPrice = document.createElement('td');
+
+    tdBicName.textContent = bicycle.bicName;
+    tdBicWheel.textContent = bicycle.bicWheel;
+    tdBicUsage.textContent = bicycle.bicUsage;
+    tdBicPrice.textContent = bicycle.bicPrice;
+
+    bicycles.append(tr);
+    tr.append(tdBicName);
+    tr.append(tdBicWheel);
+    tr.append(tdBicUsage);
+    tr.append(tdBicPrice);
+});
